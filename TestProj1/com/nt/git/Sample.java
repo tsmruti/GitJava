cd273f060e56be073a736217e2dc0dad425bce75
tsmruti <tsmruti.msf@gmail.com>
2017-04-30 01:58:14 +0530

    Update Sample.java

--- TestProj1/com/nt/git/Sample.java
+++ TestProj1/com/nt/git/Sample.java
@@ -4,6 +4,7 @@ public class Sample {
 	public void main() {
 		System.out.println("dev1 modified 1st time");
 		System.out.println("Someone modified 2nd time");
+		System.out.println("Someone modified 3rd time");
 	}
 
 }
